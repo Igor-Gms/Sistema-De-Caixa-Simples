6bd7e6c027113794fc90667ec3ee4759e7320f4e
Igor Gomes <igor.barbosa.tads@gmail.com>
2026-03-15 15:02:41 -0300

    Refactor payment selection and error handling

--- Sistema_De_Caixa.py
+++ Sistema_De_Caixa.py
@@ -1,8 +1,8 @@
+import statistics
+import os
 import datetime
-
-agora = datetime.datetime.now()
-hora = agora.strftime('%H:%M:%S')
-
+agora = datetime.datetime.now() 
+hora1 = agora.strftime('%Y-%m-%d %H:%M:%S')
 carrinho = []
 total_venda = 0
 
@@ -20,6 +20,9 @@ catalogo = {
     111: ["Refrigerante Guaraná 2L", 8.79]
 }
 
+def limpar_tela(): #Comando Para Limpar a Tela
+    os.system(cls if os.name == 'nt' else 'clear')
+
 #Mostrar Lista De Produtos
 def mostrar_catalogo():
     print("===== Catálogo De Produtos =====")
@@ -27,45 +30,61 @@ def mostrar_catalogo():
         print(f"Codigo: {codigo} | Produto: {nome} | Preço: R$ {preco:.2f}")
     print()
 
-
 mostrar_catalogo()
 
 pagamento = "N/A"
 escolha_compra = None
 
 while True:
-    carrinho1 = int(input("\nEscolha Os Produtos De Acordo Com Os Códigos (Digite 0 Para Selecionar a Forma De Pagamento): "))
+   try:
+       carrinho1 = int(input("\nEscolha Os Produtos De Acordo Com Os Códigos (Digite 0 P/ Finalizar| Negativo (-) Para Remover): "))
        #Definir Forma de Pagamento No Final Da Compra 
-    if carrinho1 == 0:
+   except :
+     print("\nOperação Inválida!")  #impede a pessoa de colocar letra e da erro
+     continue
+   if carrinho1 == 0:
+      os.system('cls' if os.name == 'nt' else 'clear') #Limpa Tela Antes De Escolher A Forma De Pagamento
+      while True:
         if len(carrinho) > 0:
             print("\nFORMAS DE PAGAMENTO:")
-            print("1 - Dinheiro - (10% De Desconto No Valor Da Compra)")
-            print("2 - Cartão de Crédito/Débito")
-            print("3 - PIX - (10% De Desconto No Valor Da Compra)")
-            escolha_compra = int(input("Escolha a forma de pagamento: "))
-            opcoes_pagamento = {1: "Dinheiro", 2: "Cartão", 3: "PIX"}
-            pagamento = opcoes_pagamento.get(escolha_compra, "Não Informado")
-        
-            print("Compra Finalizada.")
+            print("\n1 - Dinheiro - (10% De Desconto No Valor Da Compra)")
+            print("\n2 - Cartão de Crédito/Débito")
+            print("\n3 - PIX - (10% De Desconto No Valor Da Compra)")
+        try:
+             escolha_compra = int(input("\nEscolha a forma de pagamento: "))
+        except :
+            print("Digite um número das opções: Escolha a forma de pagamento") #impede a pessoa de colocar letra e da erro
+            continue
+        opcoes_pagamento = {1: "Dinheiro", 2: "Cartão", 3: "PIX"}
+        pagamento = opcoes_pagamento.get(escolha_compra, "Não Informado")
+        print("Compra Finalizada.")
         break
-      #Remover Uma Unidade Do Produto Do Carrinho
-    if carrinho1 < 0:
+      break
+    #Remover Uma Unidade Do Produto Do Carrinho
+   if carrinho1 < 0:
         codigo_para_remover = abs(carrinho1)
         if codigo_para_remover in carrinho:
             carrinho.remove(codigo_para_remover)
-            print(f"REMOVIDO: {catalogo[codigo_para_remover][0]} Saiu Do Carrinho.")
+            print(f"\nREMOVIDO: {catalogo[codigo_para_remover][0]} Saiu Do Carrinho.")
         else:
             print("Este Produto Não Está No Seu Carrinho")
 
-    elif 101 <= carrinho1 <= 111:
+   elif 101 <= carrinho1 <= 111:
         carrinho.append(carrinho1)
         print(f"\nVocê Adicionou -- {catalogo[carrinho1][0]} No Carrinho")
 
-    else:
+   else:
         print("Código Inválido")
 
 for codigo in carrinho:
     total_venda += catalogo[codigo][1]
+
+lista_precos = [] #Lista Preços dos Itens Para Cálculo da Média
+
+for codigo in carrinho:
+    preco_item = catalogo[codigo][1]
+    total_venda += preco_item
+    lista_precos.append(preco_item)
 
 # Aplicar 10% de desconto para Dinheiro ou PIX 
 desconto = 0.0
@@ -74,12 +93,26 @@ if escolha_compra == 1 or escolha_compra == 3:
 
 total_final = total_venda - desconto
 
+fim = datetime.datetime.now()
+hora2 = fim.strftime('%Y-%m-%d %H:%M:%S')
+
+os.system('cls' if os.name == 'nt' else 'clear') #Limpa Tela Antes De Mostrar Resumo Da Compra
+
 if total_venda == 0:
     print("Você Não Escolheu Nenhum Produto.")
 else:
+    print(f"Inicio: {hora1}")
+    print(f"Fim:    {hora2}")
+    print("="*40)
     print(f"Subtotal: R$ {total_venda:.2f}")
+    print("-"*40)
+    media = statistics.mean(lista_precos)
+    print(f"Média Dos Preços Por Produto: R${media:.2f}")
+    print("-"*40)
     if desconto > 0:
         print(f"Desconto (10%): - R$ {desconto:.2f}")
+        print("-"*40)
     print(f"Total da compra: R$ {total_final:.2f}")
+    print("-"*40)
     if pagamento != "N/A":
         print(f"Forma de pagamento: {pagamento}")
